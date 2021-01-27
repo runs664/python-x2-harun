09fae50a3fd162f4ce1c61e7ef3a6dae7852005c
runs-15 <harunsixsixfour@gmail.com>
2021-01-27 20:49:53 +0700

    update

--- function/faktor_bilangan.py
+++ function/faktor_bilangan.py
@@ -3,5 +3,5 @@ from functools import reduce
 def factors(n):
     return set(reduce(list.__add__,([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
 
-x = factors(90)
-print(x)
+x = input("Input angka : ")
+print(factors(x))
