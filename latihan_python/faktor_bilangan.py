3d6284e9d2d251f5739f6405b24fa1cf054dacd8
runs-15 <harunsixsixfour@gmail.com>
2021-01-27 22:04:44 +0700

    update

---
+++ latihan_python/faktor_bilangan.py
@@ -0,0 +1,7 @@
+from functools import reduce
+
+def factors(n):
+    return set(reduce(list.__add__,([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+x = int(input("Input angka : "))
+print(sorted(factors(x)))
