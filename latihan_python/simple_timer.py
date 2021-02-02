f99d8c7e6db974a4b8844b245d78f4ce044c2adc
runs-15 <harunsixsixfour@gmail.com>
2021-02-03 06:50:49 +0700

    update

--- latihan_python/simple_timer.py
+++ latihan_python/simple_timer.py
@@ -1,5 +1,5 @@
 from tqdm import tqdm   # import untuk proggress bar, sebelum itu buka cmd dan ketik 'pip install tqdm' lalu enter
 from time import sleep
 timer = int(input("Masukkan angka sebagai timer dalam satuan detik : "))
-for i in tqdm(iterable = range(timer), desc = 'Timer elapsed as percent :'):
+for i in tqdm(iterable = range(timer), desc = 'Timer elapsed as percent'):
     sleep(1)
