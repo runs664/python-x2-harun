0a25be181e4d3e40dc141ae555422794d8425167
runs-15 <harunsixsixfour@gmail.com>
2021-02-04 10:32:47 +0700

    update

---
+++ Project/Simple/Spam/README.md
@@ -0,0 +1,12 @@
+## Spam Bot 🤖
+
+※ Program ini akan membaca dan menyalin teks dari file teks yang disiapkan menuju ke media 'spam'
+
+※ <b>Disclaimer : Digunakan untuk sarana belajar coding yaaa 😋 xixi</b>
+
+※ Langkah spamming:
+
+1. Download modul <i>pyautogui</i> terlebih dahulu lewat command prompt dengan mengetikkan
+> `pip install pyautogui`
+2. Jalankan kode, tunggu selama x detik pada `time.sleep(x)` yang tertera pada kode, sambil menyiapkan kursor ketik ke media spam
+3. Voilla
--- Project/Simple/Spam/spammer.py
+++ Project/Simple/Spam/spammer.py
@@ -1,6 +1,8 @@
 import pyautogui, time
-time.sleep(7)
-f = open("Project/Simple/Spam/text.txt", 'r')
+time.sleep(7) # selama durasi sleep ini, persiapkan kursor ketik '|' ke media yang diinginkan, misal WhatsApp, Telegram, dan lain-lain
+f = open("Project/Simple/Spam/text.txt", 'r') # teks untuk bahan spam (spam akan berisi ini semua), jika ada 35 baris, maka terbuat 35 pesan
 for word in f:
-    pyautogui.typewrite(word)
-    pyautogui.press("enter")
+    pyautogui.typewrite(word) # eksekusi penyalinan file spam ke media spam
+    pyautogui.press("enter") # eksekusi enter (mengirimkan pesan)
+print("Spam selesaiiii Mweheheheheeee")
+input() # pause console
