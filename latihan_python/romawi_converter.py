239428170d601a025d524a337830f0677d0285e3
runs-15 <harunsixsixfour@gmail.com>
2021-02-05 22:24:51 +0700

    update

---
+++ latihan_python/romawi_converter.py
@@ -0,0 +1,16 @@
+def romawiToInt(romawi):
+    """
+    Mengembalikan input string berisi abjad angka romawi menjadi integer angka arab
+    \nromawi : str
+    """
+    preset = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000, 'IV':4, 'IX':9, 'XL':40, 'XC':90, 'CD':400, 'DM':900}
+    i = 0
+    num = 0
+    while i < len(romawi):
+        if i+1 < len(romawi) and romawi[i:i+2] in preset:
+            num += preset[romawi[i:i+2]]
+            i += 2
+        else:
+            num += preset[romawi[i]]
+            i += 1
+    return num
