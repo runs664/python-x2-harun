9fa7b33984ee5a721a641de6ad97cd90874a4475
runs-15 <harunsixsixfour@gmail.com>
2021-02-08 14:20:14 +0700

    update

--- Project/Simple/Spam/spammer.py
+++ Project/Simple/Spam/spammer.py
@@ -1,4 +1,3 @@
-
 import pyautogui, time
 time.sleep(7) # selama durasi sleep ini, persiapkan kursor ketik '|' ke media yang diinginkan, misal WhatsApp, Telegram, dan lain-lain
 f = open("Project/Simple/Spam/text.txt", 'r') # teks untuk bahan spam (spam akan berisi ini semua), jika ada 35 baris, maka terbuat 35 pesan
