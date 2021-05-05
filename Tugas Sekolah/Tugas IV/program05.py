4d3a27587953eae622bfd5347eec76117f46b2f4
runs-15 <harunsixsixfour@gmail.com>
2021-05-05 13:57:51 +0700

    Tugas Informatika 05/05/2021

--- Tugas Sekolah/Tugas IV/program05.py
+++ Tugas Sekolah/Tugas IV/program05.py
@@ -1,11 +1,10 @@
 def binaryToDecimal(n: int):
     result = 0
-    temp = n[::-1]
-    pemangkat = len(temp)-1
-    for x in temp:
+    pemangkat = 0
+    for x in n:
         if int(x) == 1:
-            result += 2 ** pemangkat
-        pemangkat -= 1
+            result += (2 ** pemangkat)
+        pemangkat += 1
     return result
 
 print(binaryToDecimal('11011011'))
