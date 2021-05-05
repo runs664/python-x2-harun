b68b4a4f450104dcd8f5489d3870e9182c522e25
runs-15 <harunsixsixfour@gmail.com>
2021-05-05 14:08:14 +0700

    Tugas Informatika 05/05/2021

--- Tugas Sekolah/Tugas IV/program05.py
+++ Tugas Sekolah/Tugas IV/program05.py
@@ -1,6 +1,6 @@
 def binaryToDecimal(n):
     result, pemangkat = 0, 0
-    for x in n:
+    for x in n[::-1]:
         if int(x) == 1:
             result += (2 ** pemangkat)
         pemangkat += 1
@@ -8,3 +8,5 @@ def binaryToDecimal(n):
 
 stdin = input("masukkan angka biner: ")
 print(f"hasil konversi dari {stdin} adalah {binaryToDecimal(stdin)}")
+
+#def binaryToDecimalRekursi(n):
